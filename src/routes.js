77306f0adb7d0b12d969294049dfe54cb6884e65
jacquelineturof <jacquelineturof@gmail.com>
2022-06-14 21:56:41 -0400

    get transactions route

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,8 @@ const routes = new Router();
     PLAID ROUTES
 */
 routes.get('/api/plaid/create-link', plaidController.createLinkToken);
+routes.post('/api/plaid/exchange-token', plaidController.getAccessToken);
+routes.get('/api/plaid/transactions', plaidController.getTransactions);
 
 routes.all('*', (_, res) => res.status(404).send('Page Not Found'));
 
