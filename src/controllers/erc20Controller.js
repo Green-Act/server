f51dabebf86f029cb353cd57877ad5865fe33ad5
Kevaudib <audibertikev@gmail.com>
2022-06-21 12:04:32 +0200

    feat: introduce erc20 distribution in backend
    description: introduce erc20 distribution in backend

---
+++ src/controllers/erc20Controller.js
@@ -0,0 +1,37 @@
+//interact.js
+require("dotenv").config();
+const { ethers } = require("ethers");
+const API_KEY = process.env.API_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const CONTRACT_ADDRESS = process.env.CONTRACT_ADDRESS;
+const contract = require("/Users/kevinaudiberti/Documents/GitHub/server/src/controllers/greenhacktoken.json");
+console.log(JSON.stringify(contract));
+
+
+
+// export const reward = async (amount_ghk, to) => {
+
+//     // Provider
+//     const provider = new ethers.providers.InfuraProvider(
+//     (network = "maticmum"),
+//     API_KEY
+//     );
+//     // Signer
+//     const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+//     // Contract
+//     const greenHackTokenContract = new ethers.Contract(
+//     CONTRACT_ADDRESS,
+//     contract,
+//     signer
+//     );
+
+
+//   amount = ethers.utils.parseUnits(amount_ghk, "18");
+//   const tx = await greenHackTokenContract.mint(to, amount);
+//   console.log(amount_ghk + " GHK have been minted for " + to);
+// }
+
+
+export const reward = () => {
+    console.log("Hello");
+}
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import * as plaidController from './controllers/PlaidController';
+import * as erc20Controller from './controllers/erc20Controller';
 
 const routes = new Router();
 
@@ -11,5 +12,11 @@ routes.post('/api/plaid/exchange-token', plaidController.getAccessToken);
 routes.get('/api/plaid/transactions', plaidController.getTransactions);
 
 routes.all('*', (_, res) => res.status(404).send('Page Not Found'));
+
+/*
+    ERC20 ROUTES
+*/
+routes.get('/api/erc20/reward', erc20Controller.reward);
+
 
 export default routes;
