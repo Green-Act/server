9fad72f5dbf7e9bff9ef0897040729efa25c877f
jacquelineturof <jacquelineturof@gmail.com>
2022-06-26 18:39:05 -0400

    setup database and user model

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import routes from './routes'
+import { connect } from './database'
+
+// start db connection
+connect();
 
 const PORT = process.env.PORT || 8080;
-const app = express()
+const app = express();
 initPlaid();
 
 app.use(bodyParser.urlencoded({extended: false}),);
