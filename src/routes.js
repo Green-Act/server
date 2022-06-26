f9c0ca75a3799db4c6198df7b7039c0d4b53a3f9
jacquelineturof <jacquelineturof@gmail.com>
2022-06-26 18:39:36 -0400

    add route and migrations

--- src/routes.js
+++ src/routes.js
@@ -7,8 +7,9 @@ const routes = new Router();
     PLAID ROUTES
 */
 routes.get('/api/plaid/create-link', plaidController.createLinkToken);
-routes.post('/api/plaid/exchange-token', plaidController.getAccessToken);
-routes.get('/api/plaid/transactions', plaidController.getTransactions);
+routes.post('/api/plaid/exchange-token', plaidController.createAccessToken);
+routes.get('/api/plaid/user-token/:wallet', plaidController.getAccessToken);
+routes.get('/api/plaid/transactions/:wallet', plaidController.getTransactions);
 
 routes.all('*', (_, res) => res.status(404).send('Page Not Found'));
 
